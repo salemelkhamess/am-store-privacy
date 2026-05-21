e9db3c1bad2f72fc9a0c1cb5649684310105006c
salemelkhamess <salemelkhamess@gmail.com>
2026-05-21 20:44:09 +0000

    Rem p

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,7 @@ export default function PrivacyPage() {
       content:
           "AM STORE enregistre les informations relatives aux commandes afin de permettre le suivi des achats, l’affichage de l’historique des commandes et l’amélioration de nos services.",
     },
-    {
-      title: "Paiements et sécurité",
-      content:
-          "Les informations liées aux paiements sont traitées avec des mesures de sécurité avancées afin de protéger les utilisateurs contre tout accès non autorisé ou utilisation frauduleuse.",
-    },
+
     {
       title: "Protection des données",
       content:
